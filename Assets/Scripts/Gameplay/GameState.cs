a4b0ab8f37e7adeeb06bf77d48b7e400df4bde14
Rithesh U <rithesh197rao@gmail.com>
2026-02-09 00:22:05 +0530

    Added gamestates

---
+++ Assets/Scripts/Gameplay/GameState.cs
@@ -0,0 +1,13 @@
+namespace CardDuel.Core
+{
+    public enum GameState
+    {
+        None,
+        Matchmaking,
+        Playing,
+        PendingGameOver,
+        GameOver,
+        Spectator,
+        Revealing
+    }
+}
